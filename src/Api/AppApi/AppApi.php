44be47553a9e8386eca76befd0402a0323e8ea80
Tobias Franz <cinex.mail@web.de>
2019-12-15 13:14:28 +0100

    AppApi
    * Implemented the Api
    
    Signed-off-by: Tobias Franz <cinex.mail@web.de>

--- src/Api/AppApi/AppApi.php
+++ src/Api/AppApi/AppApi.php
@@ -6,6 +6,7 @@ namespace ProxerPHP\Api\AppApi;
 
 use ProxerPHP\Api\ProxerApi;
 use ProxerPHP\Api\ProxerPHP;
+use ProxerPHP\Exception\ProxerPHPException;
 
 class AppApi extends ProxerApi {
 
@@ -14,6 +15,17 @@ class AppApi extends ProxerApi {
     public function __construct(ProxerPHP $proxerPHP) {
         parent::__construct($proxerPHP);
         $this->requestHandler = new RequestHandler($proxerPHP);
+    }
+
+    /**
+     * @param int $appId
+     * @param string $message
+     * @param bool $anonymous
+     * @param bool $silent
+     * @throws ProxerPHPException
+     */
+    public function sendErrorMessage(int $appId, string $message, bool $anonymous = true, $silent = true) {
+        $this->requestHandler->sendErrorMessage($appId, $message, $anonymous, $silent);
     }
 
 }
--- src/Api/AppApi/RequestHandler.php
+++ src/Api/AppApi/RequestHandler.php
@@ -5,7 +5,29 @@ namespace ProxerPHP\Api\AppApi;
 
 
 use ProxerPHP\Api\BaseRequestHandler;
+use ProxerPHP\Api\Methods;
+use ProxerPHP\Exception\ProxerPHPException;
 
 class RequestHandler extends BaseRequestHandler {
+
+    /**
+     * @param int $appId
+     * @param string $message
+     * @param bool $anonymous
+     * @param bool $silent
+     * @throws ProxerPHPException
+     */
+    public function sendErrorMessage(int $appId, string $message, bool $anonymous, bool $silent): void {
+        $request = $this->proxerPHP->sendPostRequest(Methods::APPS_ERRORLOG, [
+            'id'      => $appId,
+            'message' => $message,
+            'anonym'  => $anonymous,
+            'silent'  => $silent
+        ]);
+
+        if ($request->hasFailed()) {
+            throw new ProxerPHPException($request->getMessage(), $request->getCode());
+        }
+    }
 
 }
